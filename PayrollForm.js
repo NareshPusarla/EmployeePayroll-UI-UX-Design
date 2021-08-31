2cf2b8830974b5a238a61bca2492775c0ee37e16
Naresh <pnareshsatya@gmail.com>
2021-08-31 19:13:05 +0530

    Reset

--- PayrollForm.js
+++ PayrollForm.js
@@ -128,3 +128,30 @@ function createAndUpdateStorage(employeePayrollData) {
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
+
+const resetForm = () =>{
+    getSelectedValues('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name = department]');
+    setValues('#salary','');
+    setValue('#notes','');
+    setValue('#startDate','1-January-2020');
+}
+
+const unsetSelectedValues = (propertyValue)=>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item=>{
+        item.checked=false;
+    });
+}
+
+const setTextValue = (id, value)=>{
+    const element = document.querySelector(id);
+    element.textContent=value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value=value;
+}
